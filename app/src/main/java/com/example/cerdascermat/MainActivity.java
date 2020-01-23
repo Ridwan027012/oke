2c3836256991ff0803bcb514beebd414925235a3
afryannn <you@example.com>
2020-01-23 10:49:03 +0700

    first commit

---
+++ app/src/main/java/com/example/cerdascermat/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.cerdascermat;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button mRegisterBtn, mLoginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRegisterBtn = findViewById(R.id.register_btn);
+        mLoginBtn = findViewById(R.id.login_btn);
+
+        mRegisterBtn .setOnClickListener (new View.OnClickListener(){
+          @Override
+        public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+          }
+        });
+    }
+}
